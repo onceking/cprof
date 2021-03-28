486e019263cf3f8aa29581b900f5c12ba54f0e9a
Sai Ke WANG <onceking@users.noreply.github.com>
2021-03-28 12:53:16 -0400

    wip

--- __main__.py
+++ __main__.py
@@ -1,9 +1,13 @@
 #! /usr/bin/env -S python3 -u
 
+import collections
+import json
 import argparse
 import os
 import subprocess
 import shlex
+import tempfile
+import re
 
 def find_sources(paths, extensions):
     sources = set()
@@ -19,20 +23,65 @@ def find_sources(paths, extensions):
                     sources.add(os.path.join(curdir, f))
     return sorted(sources)
 
+def timed_run(cmd, **kwargs):
+    tmpfile = tempfile.NamedTemporaryFile('r')
+    cmd = [
+        '/usr/bin/time',
+        '--output', tmpfile.name,
+        '--format', re.sub(r': "(%.)"', r': \1', json.dumps({
+            'status': '%x',
+            'time': {
+                'wall': '%e',
+                'sys': '%S',
+                'user': '%U'
+            },
+            'fault': {
+                'major': '%F',
+                'minor': '%R',
+            },
+            'rss': {
+                'max': '%M',
+            }
+        }))
+    ] + cmd
+    proc = subprocess.run(cmd, **kwargs)
+    return proc, json.load(tmpfile)
+
 class Compiler:
     def __init__(self, bin_path, flags):
         self._args = [bin_path] +flags
 
-    def output(self, source, *flags):
+    def _cmd(self, source, *flags):
         cmd = self._args + list(flags) + [source]
         print(' '.join([shlex.quote(x) for x in cmd]))
+        return cmd
+
+    def includes(self, source, *flags):
+        # invalid precompiled header file is printed with ‘...x’ and a valid one with ‘...!’ .
+        ret = set()
+        for l in self.stderr(source,'-E', '-H', *flags).split('\n'):
+            m = re.search(r'^[.]+ (.+)', l.strip())
+            if m:
+                ret.add(m.group(1))
+        return ret
+
+    def stdout(self, source, *flags):
+        return self._output('stdout', source, *flags)
+    def stderr(self, source, *flags):
+        return self._output('stderr', source, *flags)
+    def _output(self, which, source, *flags):
         try:
-            return subprocess.run(
-                self._args + list(flags) + [source],
+            proc = subprocess.run(
+                self._cmd(source, *flags),
                 check=True, capture_output=True
-            ).stdout.decode('utf-8')
+            )
+            s = (proc.stderr if which == 'stderr' else proc.stdout)
+            return s.decode('utf-8')
         except subprocess.CalledProcessError as e:
             print(e.stderr.decode('utf-8'))
+
+    def time(self, source, *flags):
+        return timed_run(self._cmd(source, *flags))[1]
 
 def main():
     parser = argparse.ArgumentParser(description='Compiler Profiler')
@@ -43,9 +92,21 @@ def main():
     subparser.add_argument('paths', nargs='+')
 
     args = parser.parse_args()
-    cc = Compiler(args.bin, shlex.split(args.flags) if args.flags else [])
+    cc = Compiler(
+        args.bin,
+        (shlex.split(args.flags) if args.flags else []))
     sources = find_sources(args.paths, ['cpp'])
+    # wall_times = {}
+    # for source in sources:
+    #     t = cc.time(source, '-c')
+    #     assert t['status'] == 0
+    #     wall_times[source] = t['time']['wall']
+    # print(wall_times)
+
+    headers = collections.defaultdict(int)
     for source in sources:
-        print(cc.output(source, '-H'))
+        for header in cc.includes(source):
+            headers[header] += 1
+    print({x: y for x,y in headers.items() if y == len(sources)})
 
 main()
